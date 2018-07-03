5556b2e29a267c9eb75a21eea9a19d4c95ae8881
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-07-03 19:14:27 +0200

    Update test_unred.py, write tests for a generator and two functions from the unred.py file

--- tests/test_unred.py
+++ tests/test_unred.py
@@ -80,6 +80,51 @@ class UnredTest(unittest.TestCase):
         intersection = find_intersection((2.61, -4.32), (-7.10, 2.61))
         self.assertAlmostEqual(float(intersection), 0.71369722)
 
+    def test_point_positions(self):
+        point = (1, 2.47, -8.43, 0.056, 0.128)
+        position_iterator = point_positions(point)
+
+        for i in range(5):
+            x,y = next(position_iterator)
+
+        self.assertEqual((x, y), (2.414, -8.558))
+
+    def test_extinction(self):
+        calculations = extinction(self.stars, self.sequence,
+                                        self.reddening_line_slope, 3.1)
+        results = (4, 0.153, 0.054000000000000006, -0.007227513227513221,
+                   -0.06136380952380951, 0.16022751322751322,
+                   0.11536380952380951, 0.496705291005291)
+
+        for i,j in zip(calculations[45], results):
+            self.assertAlmostEqual(i, j)
+
+    def test_select_extinction(self):
+        calculations = extinction(self.stars, self.sequence,
+                                self.reddening_line_slope, 3.1)
+        calc_default = select_extinction(calculations)
+        calc_min = select_extinction(calculations, "min")
+        calc_max = select_extinction(calculations, "max")
+
+        default = (4, 0.12899999999999998, 0.054000000000000006,
+                   0.0021676646706587023, -0.037319281437125706,
+                   0.12683233532934127, 0.09131928143712571,
+                   0.39318023952095793)
+        minimum = (2, 0.968, 0.365, 0.6492203389830512, 0.13547864406779692,
+                   0.31877966101694877, 0.22952135593220307, 0.988216949152541)
+        maximum = (6, 0.295, 0.051000000000000004, -0.05115646258503401,
+                   -0.19823265306122448, 0.34615646258503396,
+                   0.2492326530612245, 1.0730850340136053)
+
+        for i,j in zip(calc_default[3], default):
+            self.assertAlmostEqual(i, j)
+
+        for i,j in zip(calc_min[1], minimum):
+            self.assertAlmostEqual(i, j)
+
+        for i,j in zip(calc_max[5], maximum):
+            self.assertAlmostEqual(i, j)
+
 
 if __name__ == "__main__":
     unittest.main()
